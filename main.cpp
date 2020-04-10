1db9262b1f604383a58c6f2f4dd87312687c064c
NULLCT <45321757+NULLCT@users.noreply.github.com>
2020-04-10 16:21:03 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,30 @@
-/*//////////////////////////////////////*/
-/*/  NULLCT$ (｀･ω･´)　<<"Be accept!!"  /*/
-/*//////////////////////////////////////*/
+/*////////////////////////////////////////////////////
+______________________________________________________
+    ____           __
+    /   )          / |
+---/__ /-----__---/__|----__----__----__------__--_/_-
+  /    )   /___) /   |  /   ' /   ' /___)   /   ) /
+_/____/___(___ _/____|_(___ _(___ _(___ ___/___/_(_ __
+                                          /
+                                         /
+*/////////////////////////////////////////////////////
 #include <algorithm>
 #include <iostream>
 #include <math.h>
+#include <stack>
 #include <queue>
-#include <stdio.h>
-#include <string>
+#include <list>
+#include <iterator>
+#include <cctype>
+#include <cstdio>
+#include <cstring>
 #include <vector>
 #define rep(counter, limit) for (int counter = 0; counter < limit; counter++)
-typedef long long ll; using namespace std;
+typedef long long ll; using namespace std;const int INF=1001001001;
 int gcd(int a, int b) { if (a % b == 0) { return(b); } else { return(gcd(b, a % b)); } }
 int lcm(int a, int b) { return a * b / gcd(a, b); }
 bool primenum(int a) { if (a == 1) { return false; }if (a == 2) { return true; }if (a % 2 == 0) { return false; }for (int i = 3; i < sqrt(a) + 1; i += 2) { if (a != i) { if (a % i == 0) { return false; } } }return true; }
-int main() {ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+int main() {
 
 
 
