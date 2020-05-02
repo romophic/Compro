753f7e90571c318ac1d43ca0c6a4ac1c2749d262
NULLCT <45321757+NULLCT@users.noreply.github.com>
2020-05-02 18:58:32 +0900

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,33 +1,39 @@
-//////////////////////////////////////////////////////////
-//______________________________________________________//
-//    ____           __                                 //
-//    /   )          / |                                //
-//---/__ /-----__---/__|----__----__----__------__--_/_-//
-//  /    )   /___) /   |  /   ' /   ' /___)   /   ) /   //
-//_/____/___(___ _/____|_(___ _(___ _(___ ___/___/_(_ __//
-//                                          /           //
-//                                         /            //
-//////////////////////////////////////////////////////////
-#include <algorithm>
-#include <iostream>
-#include <math.h>
-#include <stack>
-#include <queue>
-#include <deque>
-#include <list>
-#include <iterator>
-#include <cctype>
-#include <cstdio>
-#include <cstring>
-#include <vector>
+///////////////////////////////////////////////////
+/* __                                       __   */
+/*|  |--.-----.---.-.----.----.-----.-----.|  |_ */
+/*|  _  |  -__|  _  |  __|  __|  -__|  _  ||   _|*/
+/*|_____|_____|___._|____|____|_____|   __||____|*/
+/*                                  |__|         */
+///////////////////////////////////////////////////
+#include<algorithm>
+#include<bitset>
+#include<cmath>
+#include<complex>
+#include<deque>
+#include<functional>
+#include<iomanip>
+#include<iostream>
+#include<iterator>
+#include<map>
+#include<numeric>
+#include<queue>
+#include<set>
+#include<stack>
+#include<string>
+#include<unordered_map>
+#include<unordered_set>
+#include<utility>
+#include<vector>
+typedef long long lint; using namespace std;
 #define rep(counter, limit) for (int counter = 0; counter < limit; counter++)
-typedef long long ll; using namespace std;const int INF=1001001001;
+#define INF 1000000000000
+#define MOD 10000007
 int gcd(int a, int b) { if (a % b == 0) { return(b); } else { return(gcd(b, a % b)); } }
 int lcm(int a, int b) { return a * b / gcd(a, b); }
 bool primenum(int a) { if (a == 1) { return false; }if (a == 2) { return true; }if (a % 2 == 0) { return false; }for (int i = 3; i < sqrt(a) + 1; i += 2) { if (a != i) { if (a % i == 0) { return false; } } }return true; }
+struct setupios{setupios(){cin.tie(0);ios::sync_with_stdio(0); cout<<fixed<<setprecision(20);};}setupios;
 int main() {
 
 
 
-return 0;
 }
