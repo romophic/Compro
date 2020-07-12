68cc024208859ad66645ea53b9c630013d658546
nari <nari@MacBookPro.local>
2020-07-12 13:33:59 +0900

    add main.cpp and .vscode

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <array>
 #include <bitset>
 #include <cctype>
+#include <chrono>
 #include <cmath>
 #include <complex>
 #include <cstdint>
@@ -26,43 +27,46 @@
 #include <utility>
 #include <vector>
 
-#define REP(count_ver, lower, upper) for (long long count_ver = lower; count_ver < upper; count_ver++)
+#define REP(i, lower, upper) for (long long i = lower; i < upper; i++)
 
 typedef long long ll;
 using namespace std;
-const int MOD=10000007;
+const int MOD = 10000007;
 
 int gcd(int a, int b) {
-  if (a % b == 0) return (b);
-  else            return (gcd(b, a % b));
+  if (a % b == 0)
+    return (b);
+  else
+    return (gcd(b, a % b));
 }
 
 int lcm(int a, int b) { return a * b / gcd(a, b); }
 
-void divisor(long long n,vector<long long> &ret) {
+vector<long long> divisor(long long n) {
+  vector<long long> ret;
   for (long long i = 1; i * i <= n; i++) {
     if (n % i == 0) {
       ret.push_back(i);
-      if (i * i != n) ret.push_back(n / i);
+      if (i * i != n)
+        ret.push_back(n / i);
     }
   }
+  return (ret);
 }
 
 //--------------------------------
 
-void execute(){
-
+void execute() {
 }
 
 //--------------------------------
 
 int main() {
-
   cin.tie(0);
   ios::sync_with_stdio(0);
-  //cout<<fixed<<setprecision(20);
+  // cout<<fixed<<setprecision(20);
 
   execute();
 
-  return 0;
+  return (0);
 }
