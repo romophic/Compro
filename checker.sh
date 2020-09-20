6db2d5457f1b953841c4cd3dc6dcee6a08f6b8e0
NULLCT <cockatiel.u10@gmail.com>
2020-09-20 23:39:45 +0900

    fix variable bug

--- checker.sh
+++ checker.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
+noerror=true
+
 if [ ! -d "build" ]; then
+  noerror=false
   echo "ERROR: build folder not found"
   mkdir build
 fi
+
+if $noerror ; then
+  echo "No error!"
+fi
+
+echo "Check is over"
+exit
