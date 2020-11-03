00c5bb919f67db391beeca2b0672db9bdaaa1e02
NULLCT <cockatiel.u10@gmail.com>
2020-11-03 13:39:31 +0900

    fix copi.sh

--- script/copi.sh
+++ script/copi.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-stdin=`cat`
-
 if [ "$(uname)" = 'Darwin' ]; then #Mac
-  echo $stdin | pbcopy
-else #Linux
-  echo $stdin | xclip -in -sel clip
+  'cat' | pbcopy
+else                               #Linux
+  'cat' | xclip -in -sel clip
 fi
