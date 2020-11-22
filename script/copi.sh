83fb76ca7f505c4d017c9a9605017d671398af02
NULLCT <cockatiel.u10@gmail.com>
2020-11-22 20:37:54 +0900

    update copi,sh

--- script/copi.sh
+++ script/copi.sh
@@ -1,7 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ "$(uname)" = 'Darwin' ]; then #Mac
-  'cat' | pbcopy
-else                               #Linux
-  'cat' | xclip -in -sel clip
+#     ____            _    #
+#    / ___|___  _ __ (_)   #
+#   | |   / _ \| '_ \| |   #
+#   | |__| (_) | |_) | |   #
+#    \____\___/| .__/|_|   #
+#              |_|         #
+#      Made by NULLCT      #
+
+if type "clip.exe" > /dev/null 2>&1; then
+  'cat' | clip.exe
+  echo "Copy by clip"
+  exit
 fi
+
+if type "pbcopy" > /dev/null 2>&1; then
+  'cat' | pbcopy
+  echo "Copy by pbcopy"
+  exit
+fi
+
+if type "xclip" > /dev/null 2>&1; then
+  'cat' | xclip -in -sel clip
+  echo "Copy by xclip"
+  exit
+fi
+
+echo "Not found command for copy" >&2 # 404
