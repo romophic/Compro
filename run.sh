add12236d9f747ea00edac2e5967484f6f95482c
NULLCT <cockatiel.u10@gmail.com>
2022-01-22 22:51:59 +0900

    update copy function

--- run.sh
+++ run.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 
 function copy () {
+  tr=false
+
   if type "clip.exe" > /dev/null 2>&1; then
     'cat' | clip.exe
-    exit
+    tr=true
   fi
 
   if type "pbcopy" > /dev/null 2>&1; then
     'cat' | pbcopy
-    exit
+    tr=true
   fi
 
   if type "xclip" > /dev/null 2>&1; then
     'cat' | xclip -in -sel clip
-    exit
+    tr=true
   fi
 
-  echo "No command found to copy" >&2
+  if "${tr}";then
+    return 0
+  else
+    return 1
+  fi
 }
 
-copy < ./src/main.cpp &
+copy < ./src/main.cpp && echo "👍"
 mkdir build &> /dev/null
 rm -v ./build/main
 clang++ -std=c++17 -Og -g -pipe -D DEBUG ./src/main.cpp -o ./build/main && lldb ./build/main
