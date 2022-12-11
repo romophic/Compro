06db06333f775c5e59508c55f98ef7a9b81d7151
NULLCT <cockatiel.u10@gmail.com>
2022-12-12 03:45:04 +0900

    change compile system and add sanitizers

--- run.sh
+++ run.sh
@@ -1,30 +1,5 @@
 #!/bin/bash
 
-function copy () {
-  tr=false
-
-  if type "clip.exe" > /dev/null 2>&1; then
-    'cat' | clip.exe
-    tr=true
-  fi
-
-  if type "pbcopy" > /dev/null 2>&1; then
-    'cat' | pbcopy
-    tr=true
-  fi
-
-  if type "xclip" > /dev/null 2>&1; then
-    'cat' | xclip -in -sel clip
-    tr=true
-  fi
-
-  if "${tr}";then
-    return 0
-  else
-    return 1
-  fi
-}
-
-copy < ./src/main.cpp && echo "👍"
-rm a.out
+rm main
+make main
 g++ -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -fno-sanitize-recover -fstack-protector -pipe -DDEBUG  -std=c++17 -O3 ./src/main.cpp
