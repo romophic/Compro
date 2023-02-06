74cf4204191ec05e86d12aa5d8ccd6b22e62164c
NULLCT <cockatiel.u10@gmail.com>
2023-02-07 00:19:08 +0900

    add rollinghash

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,8 @@
 /*             (__)  (__)(__)(__)             */
 /*          github.com/NULLCT/Compro          */
 
-#pragma GCC target("avx2")
-#pragma GCC optimize("O3")
-#pragma GCC optimize("unroll-loops")
-#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx")
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2")
 
 #include <algorithm>
 #include <array>
@@ -37,6 +35,7 @@
 #include <map>
 #include <numeric>
 #include <queue>
+#include <random>
 #include <set>
 #include <stack>
 #include <stdexcept>
@@ -96,43 +95,26 @@ template <class T>
 ostream &operator<<(ostream &_ostr, const unordered_multiset<T> &_v);
 
 template <class T>
-void orange(ostream &_ostr, const T &_v) {
+ostream &_orange(ostream &_ostr, const T &_v) {
   if (_v.size() == 0)
-    return;
+    return _ostr;
   _ostr << *begin(_v);
   for (auto itr = next(begin(_v)), enditr = end(_v); itr != enditr; itr++)
     _ostr << " " << *itr;
+  return _ostr;
 }
 template <class T, size_t S>
-ostream &operator<<(ostream &_ostr, const array<T, S> &_v) {
-  orange(_ostr, _v);
-  return _ostr;
-}
+ostream &operator<<(ostream &_ostr, const array<T, S> &_v) { return _orange(_ostr, _v); }
 template <class T>
-ostream &operator<<(ostream &_ostr, const vector<T> &_v) {
-  orange(_ostr, _v);
-  return _ostr;
-}
+ostream &operator<<(ostream &_ostr, const vector<T> &_v) { return _orange(_ostr, _v); }
 template <class T>
-ostream &operator<<(ostream &_ostr, const valarray<T> &_v) {
-  orange(_ostr, _v);
-  return _ostr;
-}
+ostream &operator<<(ostream &_ostr, const valarray<T> &_v) { return _orange(_ostr, _v); }
 template <class T>
-ostream &operator<<(ostream &_ostr, const deque<T> &_v) {
-  orange(_ostr, _v);
-  return _ostr;
-}
+ostream &operator<<(ostream &_ostr, const deque<T> &_v) { return _orange(_ostr, _v); }
 template <class T>
-ostream &operator<<(ostream &_ostr, const list<T> &_v) {
-  orange(_ostr, _v);
-  return _ostr;
-}
+ostream &operator<<(ostream &_ostr, const list<T> &_v) { return _orange(_ostr, _v); }
 template <class T, class Y>
-ostream &operator<<(ostream &_ostr, const pair<T, Y> &_v) {
-  _ostr << _v.first << " " << _v.second;
-  return _ostr;
-}
+ostream &operator<<(ostream &_ostr, const pair<T, Y> &_v) { return _orange(_ostr, _v); }
 template <class... Ts>
 ostream &operator<<(ostream &_ostr, const tuple<Ts...> &_v) {
   bool first = true;
@@ -149,40 +131,19 @@ ostream &operator<<(ostream &_ostr, const tuple<Ts...> &_v) {
   return _ostr;
 }
 template <class T, class Y>
-ostream &operator<<(ostream &_ostr, const map<T, Y> &_v) {
-  orange(_ostr, _v);
-  return _ostr;
-}
+ostream &operator<<(ostream &_ostr, const map<T, Y> &_v) { return _orange(_ostr, _v); }
 template <class T, class Y>
-ostream &operator<<(ostream &_ostr, const multimap<T, Y> &_v) {
-  orange(_ostr, _v);
-  return _ostr;
-}
+ostream &operator<<(ostream &_ostr, const multimap<T, Y> &_v) { return _orange(_ostr, _v); }
 template <class T, class Y>
-ostream &operator<<(ostream &_ostr, const unordered_map<T, Y> &_v) {
-  orange(_ostr, _v);
-  return _ostr;
-}
+ostream &operator<<(ostream &_ostr, const unordered_map<T, Y> &_v) { return _orange(_ostr, _v); }
 template <class T>
-ostream &operator<<(ostream &_ostr, const set<T> &_v) {
-  orange(_ostr, _v);
-  return _ostr;
-}
+ostream &operator<<(ostream &_ostr, const set<T> &_v) { return _orange(_ostr, _v); }
 template <class T>
-ostream &operator<<(ostream &_ostr, const multiset<T> &_v) {
-  orange(_ostr, _v);
-  return _ostr;
-}
+ostream &operator<<(ostream &_ostr, const multiset<T> &_v) { return _orange(_ostr, _v); }
 template <class T>
-ostream &operator<<(ostream &_ostr, const unordered_set<T> &_v) {
-  orange(_ostr, _v);
-  return _ostr;
-}
+ostream &operator<<(ostream &_ostr, const unordered_set<T> &_v) { return _orange(_ostr, _v); }
 template <class T>
-ostream &operator<<(ostream &_ostr, const unordered_multiset<T> &_v) {
-  orange(_ostr, _v);
-  return _ostr;
-}
+ostream &operator<<(ostream &_ostr, const unordered_multiset<T> &_v) { return _orange(_ostr, _v); }
 
 template <class T, size_t S>
 istream &operator>>(istream &_istr, array<T, S> &_v);
@@ -195,33 +156,24 @@ istream &operator>>(istream &_istr, deque<T> &_v);
 template <class T, class Y>
 istream &operator>>(istream &_istr, pair<T, Y> &_v);
 
+template <class T>
+istream &_irange(istream &_istr, T &_v) {
+  for (auto &i : _v)
+    _istr >> i;
+  return _istr;
+}
 template <class T, size_t S>
-istream &operator>>(istream &_istr, array<T, S> &_v) {
-  for (auto &i : _v)
-    _istr >> i;
-  return _istr;
-}
+istream &operator>>(istream &_istr, array<T, S> &_v) { return _irange(_istr, _v); }
 template <class T>
-istream &operator>>(istream &_istr, vector<T> &_v) {
-  for (auto &i : _v)
-    _istr >> i;
-  return _istr;
-}
+istream &operator>>(istream &_istr, vector<T> &_v) { return _irange(_istr, _v); }
 template <class T>
-istream &operator>>(istream &_istr, valarray<T> &_v) {
-  for (auto &i : _v)
-    _istr >> i;
-  return _istr;
-}
+istream &operator>>(istream &_istr, valarray<T> &_v) { return _irange(_istr, _v); }
 template <class T>
-istream &operator>>(istream &_istr, deque<T> &_v) {
-  for (auto &i : _v)
-    _istr >> i;
-  return _istr;
-}
+istream &operator>>(istream &_istr, deque<T> &_v) { return _irange(_istr, _v); }
 template <class T, class Y>
 istream &operator>>(istream &_istr, pair<T, Y> &_v) {
-  _istr >> _v.first >> _v.second;
+  for (auto &i : _v)
+    _istr >> i.first >> i.second;
   return _istr;
 }
 
@@ -462,15 +414,14 @@ public:
   int n;
   vector<int> p;
   UnionFind(int _n) : n(_n), p(_n, -1) {}
-  int merge(int a, int b) {
+  bool merge(int a, int b) {
     int x = root(a), y = root(b);
     if (x == y)
-      return x;
+      return false;
     if (-p[x] < -p[y])
       swap(x, y);
-    p[x] += p[y];
-    p[y] = x;
-    return x;
+    p[x] += p[y],p[y]=x;
+    return true;
   }
   bool isSame(int a, int b) {
     return root(a) == root(b);
@@ -496,6 +447,56 @@ public:
       res[buf[i]].push_back(i);
     res.erase(remove_if(res.begin(), res.end(), [&](const vector<int> &v) { return v.empty(); }), res.end());
     return res;
+  }
+};
+class RollingHash {
+  static constexpr unsigned mod = 1000000007;
+
+public:
+  vector<unsigned> hashed, power;
+  inline unsigned mul(unsigned a, unsigned b) const {
+    unsigned long long x = (unsigned long long)a * b;
+    unsigned xh = (unsigned)(x >> 32), xl = (unsigned)x, d, m;
+    asm("divl %4; \n\t"
+        : "=a"(d), "=d"(m)
+        : "d"(xh), "a"(xl), "r"(mod));
+    return m;
+  }
+  RollingHash(const string &s, unsigned base = 10007) {
+    int sz = (int)s.size();
+    hashed.assign(sz + 1, 0);
+    power.assign(sz + 1, 0);
+    power[0] = 1;
+    for (int i = 0; i < sz; i++) {
+      power[i + 1] = mul(power[i], base);
+      hashed[i + 1] = mul(hashed[i], base) + s[i];
+      if (hashed[i + 1] >= mod)
+        hashed[i + 1] -= mod;
+    }
+  }
+  unsigned get(int l, int r) const {
+    unsigned ret = hashed[r] + mod - mul(hashed[l], power[r - l]);
+    if (ret >= mod)
+      ret -= mod;
+    return ret;
+  }
+  unsigned connect(unsigned h1, int h2, int h2len) const {
+    unsigned ret = mul(h1, power[h2len]) + h2;
+    if (ret >= mod)
+      ret -= mod;
+    return ret;
+  }
+  int LCP(const RollingHash &b, int l1, int r1, int l2, int r2) {
+    int len = min(r1 - l1, r2 - l2);
+    int low = -1, high = len + 1;
+    while (high - low > 1) {
+      int mid = (low + high) / 2;
+      if (get(l1, l1 + mid) == b.get(l2, l2 + mid))
+        low = mid;
+      else
+        high = mid;
+    }
+    return (low);
   }
 };
 
