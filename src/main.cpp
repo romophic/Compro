bf75e082555ca88a67e94b92079b2212c2e9b2c0
NULLCT <cockatiel.u10@gmail.com>
2024-06-16 21:23:50 +0900

    shorter stldumper

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,96 @@ template <class T>
 bool chmax(T &a, const T &b) { return a < b ? a = b, true : false; }
 template <class T>
 bool chmin(T &a, const T &b) { return a > b ? a = b, true : false; }
+template <class T>
+ostream &operator<<(ostream &_ostr, const vector<T> &_v);
+template <class T>
+ostream &operator<<(ostream &_ostr, const deque<T> &_v);
+template <class T>
+ostream &operator<<(ostream &_ostr, const list<T> &_v);
+template <class T, class Y>
+ostream &operator<<(ostream &_ostr, const pair<T, Y> &_v);
+template <class... Ts>
+ostream &operator<<(ostream &_ostr, const tuple<Ts...> &t);
+template <class T, class Y>
+ostream &operator<<(ostream &_ostr, const map<T, Y> &_v);
+template <class T, class Y>
+ostream &operator<<(ostream &_ostr, const multimap<T, Y> &_v);
+template <class T, class Y>
+ostream &operator<<(ostream &_ostr, const unordered_map<T, Y> &_v);
+template <class T, class Y>
+ostream &operator<<(ostream &_ostr, const unordered_multimap<T, Y> &_v);
+template <class T>
+ostream &operator<<(ostream &_ostr, const set<T> &_v);
+template <class T>
+ostream &operator<<(ostream &_ostr, const multiset<T> &_v);
+template <class T>
+ostream &operator<<(ostream &_ostr, const unordered_set<T> &_v);
+template <class T>
+ostream &operator<<(ostream &_ostr, const unordered_multiset<T> &_v);
+template <class T>
+ostream &_orange(ostream &_ostr, const T &_v) {
+  if (_v.size() == 0)
+    return _ostr;
+  _ostr << *begin(_v);
+  for (auto itr = next(begin(_v)), enditr = end(_v); itr != enditr; itr++)
+    _ostr << " " << *itr;
+  return _ostr;
+}
+template <class T>
+ostream &operator<<(ostream &_ostr, const vector<T> &_v) { return _orange(_ostr, _v); }
+template <class T>
+ostream &operator<<(ostream &_ostr, const deque<T> &_v) { return _orange(_ostr, _v); }
+template <class T>
+ostream &operator<<(ostream &_ostr, const list<T> &_v) { return _orange(_ostr, _v); }
+template <class T, class Y>
+ostream &operator<<(ostream &_ostr, const pair<T, Y> &_v) { return _ostr << _v.first << ":" << _v.second; }
+template <class... Ts>
+ostream &operator<<(ostream &_ostr, const tuple<Ts...> &_v) {
+  bool first = true;
+  apply([&_ostr, &first](auto &&...args) {
+    auto print = [&](auto &&val) {
+      if (!first)
+        _ostr << " ";
+      (_ostr << val);
+      first = false;
+    };
+    (print(args), ...);
+  },
+        _v);
+  return _ostr;
+}
+template <class T, class Y>
+ostream &operator<<(ostream &_ostr, const map<T, Y> &_v) { return _orange(_ostr, _v); }
+template <class T, class Y>
+ostream &operator<<(ostream &_ostr, const multimap<T, Y> &_v) { return _orange(_ostr, _v); }
+template <class T, class Y>
+ostream &operator<<(ostream &_ostr, const unordered_map<T, Y> &_v) { return _orange(_ostr, _v); }
+template <class T>
+ostream &operator<<(ostream &_ostr, const set<T> &_v) { return _orange(_ostr, _v); }
+template <class T>
+ostream &operator<<(ostream &_ostr, const multiset<T> &_v) { return _orange(_ostr, _v); }
+template <class T>
+ostream &operator<<(ostream &_ostr, const unordered_set<T> &_v) { return _orange(_ostr, _v); }
+template <class T>
+ostream &operator<<(ostream &_ostr, const unordered_multiset<T> &_v) { return _orange(_ostr, _v); }
+template <class T>
+istream &operator>>(istream &_istr, vector<T> &_v);
+template <class T>
+istream &operator>>(istream &_istr, deque<T> &_v);
+template <class T, class Y>
+istream &operator>>(istream &_istr, pair<T, Y> &_v);
+template <class T>
+istream &_irange(istream &_istr, T &_v) {
+  for (auto &i : _v)
+    _istr >> i;
+  return _istr;
+}
+template <class T>
+istream &operator>>(istream &_istr, vector<T> &_v) { return _irange(_istr, _v); }
+template <class T>
+istream &operator>>(istream &_istr, deque<T> &_v) { return _irange(_istr, _v); }
+template <class T, class Y>
+istream &operator>>(istream &_istr, pair<T, Y> &_v) { return _istr >> _v.first >> _v.second; }
 vector<int> divisor(int n) {
   vector<int> head, tail;
   for (int i = 1; i * i <= n; i++) {
@@ -484,4 +574,6 @@ struct init {
     cout << fixed << setprecision(16);
   }
 } init;
-signed main() {}
+signed main() {
+
+}
